f998f26e0218862f86c1da3b5fff0f45112587ff
Snehil <cascadenienuo@gmail.com>
2022-03-31 17:47:21 +0530

    updated API_KEY

--- app/src/main/java/com/lava/newsapp/util/Constants.kt
+++ app/src/main/java/com/lava/newsapp/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.lava.newsapp.util
 
 object Constants {
-        const val API_KEY = "e0ed1d9a0f954cfb94f447c123b9e053"
+        const val API_KEY = "eaa97433acdb486bb14a11d8832a0f4e"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
